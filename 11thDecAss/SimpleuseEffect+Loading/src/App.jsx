a548e76229805eb06f1f45fffd9c202b258a384e
kpriyanka <kusupriyanka04@gmail.com>
2025-12-12 15:25:00 +0530

    Simple useEffect + Loading

---
+++ 11thDecAss/SimpleuseEffect+Loading/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UserData from './UserData';
+
+function App() {
+  
+
+  return (
+    <>
+       <UserData/>
+    </>
+  );
+}
+
+export default App;
---
+++ 11thDecAss/SimpleuseEffect+Loading/src/UserData.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+function UserData(){
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then(resourse => resourse.json())
+        .then(data=>{
+            setUser(data);
+            setLoading(false);
+        });
+    }, []);
+
+    if(loading){
+        return <h3>loading....</h3>;
+    }
+
+    return(
+        <div>
+            <h2>User Data</h2>
+            <p><b>Name: </b>{user.name}</p>
+            <p><b>Email: </b>{user.email}</p>
+            <p><b>Phone: </b>{user.phone}</p>
+        </div>
+    );
+}
+export default UserData;
